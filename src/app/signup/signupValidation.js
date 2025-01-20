177072c2610884e01a66e8eb75470aa8eddd9251
NabinaUpadhyaya <nabinasilwal15@gmail.com>
2025-01-20 19:27:26 +0545

    changes

---
+++ src/app/signup/signupValidation.js
@@ -0,0 +1,42 @@
+function Validation(values) {
+    let errors = {};
+    //for username
+    if (values.Name === "") {
+      errors.Name = "(Empty Field)";
+    } else if (values.Name && values.Name.trim().length === 0) {
+      errors.Name = "(Empty Field)";
+    }
+  
+    // //for username
+    // if (values.lastName === "") {
+    //   errors.lastName = "(Empty Field)";
+    // } else if (values.lastName && values.lastName.trim().length === 0) {
+    //   errors.lastName = "(Empty Field)";
+    // }
+  
+    //for email
+    let email_pattern =
+      /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (values.email === "") {
+      errors.email = "(Empty Field)";
+    } else if (values.email && values.email.trim().length === 0) {
+      errors.email = "(Empty Field)";
+    } else if (!email_pattern.test(values.email)) {
+      errors.email = "(Invalid Email)";
+    }
+  
+    //for password
+    let password_pattern = /^(?=.*[0-9])(?=.*[!@#$%^&])[a-zA-Z0-9!@#$%^&]{6,16}$/;
+  
+    // Validate the password
+    if (!values.password || values.password.trim() === "") {
+      errors.password = "(Empty Field)";
+    } else if (!password_pattern.test(values.password)) {
+      errors.password = "(Invalid Password)";
+    }
+    
+  
+    return errors;
+  }
+  
+  export default Validation;
